9e155a5f0888c21a603351296d4be3a7c04e7f9e
Istvan Laczovics <istvanl@topdesk.com>
2023-02-16 10:07:33 +0100

    Add proper naming

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { LionButton as OldButton } from '@lion/button';
-import { LionButton as newButton } from '@lion/ui/exports/button';
-import { LionButton as localLionButton } from 'local-lion-ui/exports/button';
+import { LionButton as LionButton } from '@lion/button';
+import { LionButton as LionUiButton } from '@lion/ui/exports/button.js';
+import { LionButton as LocalLionUiButton } from 'local-lion-ui/exports/button';
 import { LocalButton } from 'local-button';
 
-customElements.define('old-button', OldButton);
-customElements.define('new-button', newButton);
+customElements.define('lion-button', LionButton);
+customElements.define('lion-ui-button', LionUiButton);
+customElements.define('local-lion-ui-button', LocalLionUiButton);
 customElements.define('local-button', LocalButton);
-customElements.define('local-lion-button', localLionButton);
 
 
 
