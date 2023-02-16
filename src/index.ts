e6d89528554a11fd9d85b1c423c3b93fc4631aab
Istvan Laczovics <istvanl@topdesk.com>
2023-02-16 10:42:09 +0100

    Fixed wrong imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { LionButton as LionButton } from '@lion/button';
-import { LionButton as LionUiButton } from '@lion/ui/exports/button.js';
+// import { LionButton as LionUiButton } from '@lion/ui/exports/button.js';
 import { LionButton as LocalLionUiButton } from 'local-lion-ui/exports/button';
 import { LocalButton } from 'local-button';
 
 customElements.define('lion-button', LionButton);
-customElements.define('lion-ui-button', LionUiButton);
+// customElements.define('lion-ui-button', LionUiButton);
 customElements.define('local-lion-ui-button', LocalLionUiButton);
 customElements.define('local-button', LocalButton);
 
